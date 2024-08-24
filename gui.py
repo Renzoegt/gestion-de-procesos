4bb30bbad4c7fb97d2e5cae59e949da9eeebf999
Renzoegt <150016933+Renzoegt@users.noreply.github.com>
2024-08-24 20:39:42 -0300

    Update gui.py

---
+++ gui.py
@@ -0,0 +1,9 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("blue")
+
+app=customtkinter.CTk()
+app.title("Medina's funky time")
+app.geometry("800x500")
+app.mainloop()
