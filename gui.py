c3803580ecf2721a1e3f7e2498d41ce194e1341f
Zolmex <zolmexyt@gmail.com>
2024-08-24 22:18:54 -0300

    Clases de gestores, SO, lógica de procesos
    - Creación de clases SistemaOperativo, GestorProcesos, GestorMemoria, PCB.
    - Implementación lógica de ejecución de procesos y detención de procesos tras un tiempo limite.

--- gui.py
+++ gui.py
@@ -1,6 +1,16 @@
+class PCB():
+    def __init__(self, pid, prioridad, memoria, tiempoLimite):
+        self.pid = pid
+        self.estado = 0 # 0: Listo, 1: En ejecución, 2: Bloqueado, 3: Terminado
+        self.prioridad = prioridad
+        self.memoria = memoria
+        self.tiempoLimite = tiempoLimite
+        self.tiempoEjecucion = 0 # Tiempo en segundos
+
 class Nodo():
-    def __init__(self):
+    def __init__(self, dato:PCB):
         self.siguiente = None
+        self.dato = dato
 
 class Cola():
     def __init__(self):
@@ -45,3 +55,98 @@ class Cola():
             actual = actual.siguiente
 
         return None
+
+class GestorMemoria():
+    def __init__(self):
+        self.memoriaTotal = 500000
+        self.memoriaDisponible = self.memoriaTotal
+
+    def asignarMemoria(self, pcb:PCB):
+        if pcb.memoria > self.memoriaDisponible:
+            return False
+        
+        self.memoriaDisponible -= pcb.memoria
+        return True
+    
+    def liberarMemoria(self, pcb:PCB):
+        self.memoriaDisponible += pcb.memoria
+
+class GestorProcesos():
+    def __init__(self):
+        self.contexto = None
+        self.espera = list() # Procesos recien creados y esperando que se libere memoria
+        self.ejecucion = list() # Procesos ejecutandose concurrentemente
+
+    def ponerEnEspera(self, pcb:PCB):
+        pcb.estado = 0
+        if pcb in self.ejecucion:
+            self.ejecucion.remove(pcb)
+        self.espera.append(pcb)
+
+    def ponerEnEjecucion(self, pcb:PCB):
+        pcb.estado = 1
+        if pcb in self.espera:
+            self.espera.remove(pcb)
+        self.ejecucion.append(pcb)
+
+    def detenerProceso(self, pcb:PCB):
+        pcb.estado = 3
+        self.ejecucion.remove(pcb) # Removemos de la lista actual
+
+    def ejecutarCiclo(self, memoria:GestorMemoria):
+        if self.contexto == None:
+            return
+        
+        i = 0
+        while i < self.espera.count: # Agregar los procesos a ejecución si hay suficiente memoria
+            pcb:PCB = self.ejecucion[i]
+            
+            if memoria.asignarMemoria(pcb) == True:
+                self.ponerEnEjecucion(pcb)
+            else: # Incrementamos el índice sólo si no se removieron elementos de la lista
+                i += 1
+
+        i = 0
+        while i < self.ejecucion.count: # Simulamos la ejecución de todos los procesos en ejecución y terminamos los procesos según su tiempo límite
+            pcb:PCB = self.ejecucion[i]
+            pcb.tiempoEjecucion += 1
+            
+            if pcb.tiempoEjecucion >= pcb.tiempoLimite: # Liberamos la memoria y detenemos el proceso
+                self.detenerProceso(pcb)
+                memoria.liberarMemoria(pcb)
+            else:
+                i += 1
+    
+    def detenerProceso(self, idProceso):
+        for pcb in self.ejecucion:
+            if pcb.pid == idProceso:
+                self.detenerProceso(pcb)
+                return
+            
+        print("El PID solicitado no se encuentra en ejecución.")
+
+
+class SistemaOperativo():
+    def __init__(self):
+        self.procesos = GestorProcesos()
+        self.memoria = GestorMemoria()
+    
+    def ejecutarCiclo(self): # Llamar esta función cada segundo. Aca se ejecutan los procesos.
+        self.procesos.ejecutarCiclo(self.memoria)
+        return
+
+    def ejecutarProceso(self):
+        return 0
+    
+    def detenerProceso(self, idProceso):
+        self.procesos.detenerProceso(idProceso)
+
+# El Sistem Operativo debería iniciar cuando un usuario presione un boton de encendido
+# Esa acción iniciaría el loop infinito que representa la ejecución del proceso principal del sistema
+
+cerrar = False
+while (cerrar == False): # (Temporal) El usuario debería presionar un botón de cerrar o cerrar la ventana para que termine de ejecutarse el programa
+    print("...")
+    
+    if input() != None: # (Temporal) Cerrar programa
+        break
